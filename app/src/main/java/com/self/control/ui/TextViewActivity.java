f45457081d71d3cd30baeb1384ad3aa9e2a110c8
Your Name <you@example.com>
2019-01-14 14:05:00 +0800

    TextView

--- app/src/main/java/com/self/control/ui/TextViewActivity.java
+++ app/src/main/java/com/self/control/ui/TextViewActivity.java
@@ -3,6 +3,7 @@ package com.self.control.ui;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
@@ -39,6 +40,14 @@ public class TextViewActivity extends BaseActivity {
     TextView tvParagraph;
     @BindView(R.id.tv_aplication)
     TextView tvAplication;
+    @BindView(R.id.tv_line_bottom_all)
+    TextView tvLineBottomAll;
+    @BindView(R.id.tv_in_line_all)
+    TextView tvInLineAll;
+    @BindView(R.id.tv_in_line_all_clear)
+    TextView tvInLineAllClear;
+    @BindView(R.id.tv_hickening)
+    TextView tvHickening;
 
     private SpannableStringBuilder span;
     private SpannableString spannableString;
@@ -73,17 +82,17 @@ public class TextViewActivity extends BaseActivity {
     public void initView(View view) {
         //部分字体颜色
         spannableString = new SpannableString(tvColor.getText().toString());
-        spannableString.setSpan(new ForegroundColorSpan(Color.RED), 7,8,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new ForegroundColorSpan(Color.GREEN), 9,10,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new ForegroundColorSpan(Color.BLUE), 11,12,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new ForegroundColorSpan(Color.RED), 20,21,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new ForegroundColorSpan(Color.YELLOW), 22,23,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.young)), 24,25,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED), 7, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.GREEN), 9, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.BLUE), 11, 12, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED), 20, 21, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.YELLOW), 22, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.young)), 24, 25, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvColor.setText(spannableString);
 
         //部分字体放大
         spannableString = new SpannableString(tvBig.getText().toString());
-        spannableString.setSpan(new AbsoluteSizeSpan(50), 14,16,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new AbsoluteSizeSpan(50), 14, 16, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvBig.setText(spannableString);
 
         //添加图片
@@ -99,12 +108,12 @@ public class TextViewActivity extends BaseActivity {
                 R.mipmap.ic_launcher_round);
         //文字中间添加图片
         spannableString = new SpannableString(tvImage.getText().toString());
-        spannableString.setSpan(new ImageSpan(this, R.mipmap.ic_launcher_round), 7,8,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ImageSpan(this, R.mipmap.ic_launcher_round), 7, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvImage.setText(spannableString);
 
         //局部布局背景
         spannableString = new SpannableString(tvBackground.getText().toString());
-        spannableString.setSpan(new BackgroundColorSpan(Color.RED),7,9,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new BackgroundColorSpan(Color.RED), 7, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvBackground.setText(spannableString);
 
         //划重点，删除划线
@@ -122,25 +131,40 @@ public class TextViewActivity extends BaseActivity {
                 //设置点击事件
                 Toast.makeText(TextViewActivity.this, "重点", Toast.LENGTH_SHORT).show();
             }
-        }, 4,6,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }, 4, 6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvLine.setText(spannableString);
         tvLine.setHighlightColor(Color.TRANSPARENT); //设置点击后的颜色为透明，否则会一直出现高亮（背景）
         tvLine.setMovementMethod(LinkMovementMethod.getInstance());//设置响应点击事件
 
+        //所有文字下划线
+        tvLineBottomAll.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
+        tvLineBottomAll.getPaint().setAntiAlias(true);//抗锯齿
+
+        //所有文字中划线
+        tvInLineAll.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        tvInLineAll.getPaint().setAntiAlias(true);
+
+        //所有文字中划线（加清晰）
+        tvInLineAllClear.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG|Paint.ANTI_ALIAS_FLAG);
+        //tvInLineAllClear.getPaint().setAntiAlias(true);
+
+        //文字加粗
+        tvHickening .getPaint().setFakeBoldText(true);
+
         //首行缩进(隐藏前面两个字)
-        span = new SpannableStringBuilder("缩进"+tvParagraph.getText());
+        span = new SpannableStringBuilder("缩进" + tvParagraph.getText());
         span.setSpan(new ForegroundColorSpan(Color.TRANSPARENT), 0, 2, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         tvParagraph.setText(span);
 
         //整体应用
-        span = new SpannableStringBuilder("缩进"+tvAplication.getText());
+        span = new SpannableStringBuilder("缩进" + tvAplication.getText());
         span.setSpan(new ForegroundColorSpan(Color.TRANSPARENT), 0, 2, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         tvAplication.setText(span);
         spannableString = new SpannableString(tvAplication.getText().toString());
-        spannableString.setSpan(new ForegroundColorSpan(Color.RED),15,17,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new AbsoluteSizeSpan(55), 22,25,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new ImageSpan(this, R.mipmap.ic_launcher_round), 7,8,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        spannableString.setSpan(new BackgroundColorSpan(Color.RED),27,29,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED), 15, 17, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new AbsoluteSizeSpan(55), 22, 25, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ImageSpan(this, R.mipmap.ic_launcher_round), 7, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new BackgroundColorSpan(Color.RED), 27, 29, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         spannableString.setSpan(new ClickableSpan() {
             @Override
             public void updateDrawState(TextPaint ds) {
@@ -154,20 +178,19 @@ public class TextViewActivity extends BaseActivity {
                 //设置点击事件
                 Toast.makeText(TextViewActivity.this, "也是重点", Toast.LENGTH_SHORT).show();
             }
-        }, tvAplication.length()-5,tvAplication.length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }, tvAplication.length() - 5, tvAplication.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvAplication.setHighlightColor(Color.TRANSPARENT); //设置点击后的颜色为透明，否则会一直出现高亮（背景）
         tvAplication.setMovementMethod(LinkMovementMethod.getInstance());//设置响应点击事件
         tvAplication.setText(spannableString);
         tvAplication.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                        R.mipmap.ic_launcher_round,
-                        R.mipmap.ic_launcher_round,//getResources().getDrawable(R.mipmap.ic_launcher_round)
-                        R.mipmap.ic_launcher_round,
-                        R.mipmap.ic_launcher_round);
+                R.mipmap.ic_launcher_round,
+                R.mipmap.ic_launcher_round,//getResources().getDrawable(R.mipmap.ic_launcher_round)
+                R.mipmap.ic_launcher_round,
+                R.mipmap.ic_launcher_round);
     }
 
     @Override
     public void doBusiness(Context mContext) {
 
     }
-
 }
